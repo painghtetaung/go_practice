079b2f097bb38ef44000297b05ed4f027f5208b7
kopaing-nuos <kopaing@globalmagicko.com>
2025-05-19 15:38:06 +0700

    go buffered channel test

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	var c = make(chan int)
+	var c = make(chan int, 5)
 	go process(c)
 	for i := range c {
 		fmt.Println(i)
+		time.Sleep(time.Second * 1)
 	}
 }
 
@@ -17,4 +19,5 @@ func process(c chan int) {
 	for i := 0; i < 5; i++ {
 		c <- i
 	}
+	fmt.Println("Exiting process")
 }
