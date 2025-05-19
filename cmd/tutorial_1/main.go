7b177a820898dd0a66cff9a1ef5bdfd112c5d0e1
kopaing-nuos <kopaing@globalmagicko.com>
2025-05-19 15:22:01 +0700

    go unbuffered channel test

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 )
 
-
-
 func main() {
 	var c = make(chan int)
-	c <- 1
-	var i = <- c
-	fmt.Println(i)
+	go process(c)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
+
+func process(c chan int) {
+	defer close(c)
+	for i := 0; i < 5; i++ {
+		c <- i
+	}
 }
