b862fd5e712f3ec8b91263825684265c42fb2101
kopaing-nuos <kopaing@globalmagicko.com>
2025-05-20 11:24:55 +0700

    go methods

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -2,32 +2,29 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 var MAX_CHICKEN_PRICE float32 = 5
 
+type data struct {
+	name string
+	age int
+}
+
+func (d data) displayName() {
+	fmt.Printf("\nName: %s, Age: %d", d.name, d.age)
+}
+
+func (d *data) setAge(age int) {
+	d.age = age
+}
+
 func main() {
-	var chickenChannel = make(chan string)
-	var websites = []string{"walmart.com", "costco.com", "wholefoods.com"}
-	for i := range websites {
-		go checkChickenPrices(websites[i], chickenChannel)
-	}
-	sendMessage(chickenChannel)
-}
-
-func checkChickenPrices(website string, chickenChannel chan string) {
-	for {
-		time.Sleep(time.Second * 1)
-		var chickenPrice = rand.Float32() * 20
-		if chickenPrice <= MAX_CHICKEN_PRICE {
-			chickenChannel <- website
-			break
-		}
-	}
-}
-
-func sendMessage(chickenChannel chan string) {
-	fmt.Printf("\nFound a deal on chicken at %s", <-chickenChannel)
+	var d = data{name: "John", age: 20}
+	f1 := d.displayName
+	d.name = "Jane"
+	f2 := d.setAge
+	f2(21)
+	fmt.Println("After setAge", d)
+	f1()
 }
